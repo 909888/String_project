4c3afd49cde005fd1de34805668f553cd65f61cd
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-08-28 18:00:46 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // EX: 1 Concatenate three strings.
+            //string aboutMe = "My name is ed,";
+            //string aboutAge = " And I am 100 years old!";
+            //string aboutLife = " I also like long walks on the beach!";
+
+            //Console.WriteLine(aboutMe + aboutAge + aboutLife);
+            //Console.ReadLine();
+
+            // EX: 2 Convert a string to uppercase.
+            //string myFavcar = "ferrari";
+
+            //myFavcar = myFavcar.ToUpper();
+
+            //Console.WriteLine(myFavcar);
+            //Console.ReadLine();
+
+            // EX: 3 Create a Stringbuilder and build a paragraph of text, one sentence at a time.
+            //StringBuilder li = new StringBuilder();
+
+            //li.Append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+            //Console.WriteLine(li);
+            //Console.ReadLine();
+
+        }
+    }
+}
